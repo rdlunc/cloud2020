ef32c31e6eef59de02e23c2acdd6c8f299c7b157
Allen_glb <goodMorning_glb@rongda.com>
2020-06-04 09:16:58 +0800

    Rest微服务工程构建(支付模块) 1.建module； 2.改POM； 3.写YML; 4.主启动； 5.业务类(待补充)；

---
+++ cloud-provider-payment8001/src/main/java/com/rongda/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.rongda.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lunrongda
+ * @create 2020-06-03 23:56
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
