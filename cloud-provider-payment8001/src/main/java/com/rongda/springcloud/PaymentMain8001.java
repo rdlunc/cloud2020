db826904295e187b5bebdd0f791a2ce65045f725
Allen_glb <goodMorning_glb@rongda.com>
2020-07-13 22:11:48 +0800

    支付微服务8001入驻进eurekaServer

--- cloud-provider-payment8001/src/main/java/com/rongda/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/rongda/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.rongda.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lunrongda
  * @create 2020-06-03 23:56
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
