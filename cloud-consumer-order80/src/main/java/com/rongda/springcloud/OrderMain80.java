e5b5cbf2b7788ca3040ea1e72af38eca96bdfa5b
Allen_glb <goodMorning_glb@rongda.com>
2020-09-13 20:45:31 +0800

    订单微服务80入驻进eurekaServer

--- cloud-consumer-order80/src/main/java/com/rongda/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/rongda/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.rongda.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lunrongda
  * @create 2020-06-30 16:17
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
