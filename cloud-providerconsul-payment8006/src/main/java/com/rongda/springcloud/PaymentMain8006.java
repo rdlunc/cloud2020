97db88b0723a7780986a13e53485e6b26ada678d
Allen_glb <goodMorning_glb@rongda.com>
2020-10-09 00:36:06 +0800

    添加注释

--- cloud-providerconsul-payment8006/src/main/java/com/rongda/springcloud/PaymentMain8006.java
+++ cloud-providerconsul-payment8006/src/main/java/com/rongda/springcloud/PaymentMain8006.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @create 2020-10-08 12:08
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient // 该注解用于向使用consul或者zookeeper作为注册中心时注册服务
 public class PaymentMain8006 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8006.class, args);
