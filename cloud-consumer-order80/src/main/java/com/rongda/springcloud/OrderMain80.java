c5a0815498d1cd6454201027956d2b424df89ac6
Allen_glb <goodMorning_glb@rongda.com>
2020-12-29 23:33:58 +0800

    Ribbon负载规则替换

--- cloud-consumer-order80/src/main/java/com/rongda/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/rongda/springcloud/OrderMain80.java
@@ -1,15 +1,19 @@
 package com.rongda.springcloud;
 
+import com.rongda.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author lunrongda
  * @create 2020-06-30 16:17
+ * 在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效，形如：
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
