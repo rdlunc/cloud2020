4a2776ad12d2adf9cb57d1e20f502363fd4f0b97
Allen_glb <goodMorning_glb@rongda.com>
2021-02-10 17:17:02 +0800

    OpenFeign超时控制（补充payment8002的调试超时的方法）

--- cloud-provider-payment8002/src/main/java/com/rongda/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/rongda/springcloud/controller/PaymentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author lunrongda
@@ -51,6 +52,17 @@ public class PaymentController {
 
     @GetMapping(value = "payment/lb")
     public String getPaymentLB(){
+        return serverPort;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        //暂停3秒钟线程（模拟长流程业务调用返回）
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         return serverPort;
     }
 }
