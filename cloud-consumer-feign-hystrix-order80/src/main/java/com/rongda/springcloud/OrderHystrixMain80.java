6c9313ca88ce8a178147acc9d45e46d8bec3fa7b
Allen_glb <goodMorning_glb@rongda.com>
2021-02-16 12:34:53 +0800

    订单微服务调用支付服务出现卡顿(看热闹不嫌弃事大，80新建加入)

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/rongda/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,17 @@
+package com.rongda.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author lunrongda
+ * @create 2021-02-14 12:02
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/rongda/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,32 @@
+package com.rongda.springcloud.controller;
+
+import com.rongda.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author lunrongda
+ * @create 2021-02-14 22:55
+ */
+@RestController
+@Slf4j
+public class OrderHystrixController {
+
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id){
+        return paymentHystrixService.paymentInfo_OK(id);
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        return paymentHystrixService.paymentInfo_TimeOut(id);
+    }
+
+}
