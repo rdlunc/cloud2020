2c8197c9078936294896f0462957b1e4c10968b5
Allen_glb <goodMorning_glb@rongda.com>
2021-02-18 15:16:41 +0800

    Hystrix图形化Dashboard搭建

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/rongda/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.rongda.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author lunrongda
+ * @create 2021-02-18 15:02
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
