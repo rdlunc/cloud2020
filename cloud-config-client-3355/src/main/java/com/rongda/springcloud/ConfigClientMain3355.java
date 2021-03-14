b9c37473ed0eb75eba38722f2ad0c539ba738311
rdlunc <348799577@qq.com>
2021-03-14 19:01:02 +0800

    Config客户端配置与测试

---
+++ cloud-config-client-3355/src/main/java/com/rongda/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.rongda.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lunrongda
+ * @create 2021-03-14 18:09
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/rongda/springcloud/Controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.rongda.springcloud.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lunrongda
+ * @create 2021-03-14 18:17
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
