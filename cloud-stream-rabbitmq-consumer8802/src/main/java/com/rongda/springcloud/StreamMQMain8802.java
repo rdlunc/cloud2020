d2a055d8c186aeb1cb8ff3963fb9e4d975cfe209
rdlunc <348799577@qq.com>
2021-06-18 09:05:57 +0800

    Stream消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/rongda/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.rongda.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lunrongda
+ * @create 2021-06-17 23:18
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
