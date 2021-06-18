8fe89c72ac902404b0d8e908430c55b83d59d43c
rdlunc <348799577@qq.com>
2021-06-18 17:38:32 +0800

    删除没用的注释

--- cloud-stream-rabbitmq-consumer8802/src/main/java/com/rongda/springcloud/controller/ReceiveMessageListenerController.java
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/rongda/springcloud/controller/ReceiveMessageListenerController.java
@@ -17,14 +17,6 @@ import org.springframework.stereotype.Component;
 @EnableBinding(Sink.class)
 public class ReceiveMessageListenerController {
 
-//    @Value("${server.port}")
-//    private String serverPort;
-//
-//    @StreamListener(Sink.INPUT)
-//    public void input(Message<String> message){
-//        log.info("消费者1号，======> 接受到的消息： " + message.getPayload() + "\t port: " + serverPort);
-//    }
-
     @Value("${server.port}")
     private String serverPort;
 
