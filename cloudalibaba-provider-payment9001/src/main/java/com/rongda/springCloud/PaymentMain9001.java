9b1eff2a10ee5a9833cd54895a41aa2d19aa996d
rdlunc <348799577@qq.com>
2021-06-22 09:19:39 +0800

    Nacos之服务提供者注册

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/rongda/springCloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.rongda.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lunrongda
+ * @create 2021-06-22 0:34
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/rongda/springCloud/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package com.rongda.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lunrongda
+ * @create 2021-06-22 0:34
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
