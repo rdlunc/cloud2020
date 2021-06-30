e9fc6b34a8da668de7d93799be3c9380e2c79436
rdlunc <348799577@qq.com>
2021-06-30 09:55:12 +0800

    Sentinel流控-线程数直接失败 测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/rongda/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/rongda/springcloud/controller/FlowLimitController.java
@@ -3,6 +3,8 @@ package com.rongda.springcloud.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author lunrongda
  * @create 2021-06-29 21:13
@@ -17,6 +19,12 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB(){
+        //暂停1秒
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "------------> testB";
     }
 
