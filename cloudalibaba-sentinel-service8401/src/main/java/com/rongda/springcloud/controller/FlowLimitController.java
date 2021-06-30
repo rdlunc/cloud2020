2e059df25dc9aea5e444ce2be4425b5fe9417e25
rdlunc <348799577@qq.com>
2021-06-30 15:06:30 +0800

    测试: Sentinel流控-关联 Sentinel流控-预热 Sentinel流控-排队等待

--- cloudalibaba-sentinel-service8401/src/main/java/com/rongda/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/rongda/springcloud/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.rongda.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +11,23 @@ import java.util.concurrent.TimeUnit;
  * @create 2021-06-29 21:13
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA(){
+        log.info(Thread.currentThread().getName() + "\t" + "---------> testA");
         return "------------> testA";
     }
 
     @GetMapping("/testB")
     public String testB(){
         //暂停1秒
-        try {
-            TimeUnit.MILLISECONDS.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return "------------> testB";
     }
 
