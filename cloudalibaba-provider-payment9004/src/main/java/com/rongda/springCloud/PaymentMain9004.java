62496b638edad25c6c52f1ab9aec744946298ede
rdlunc <348799577@qq.com>
2021-07-04 16:46:14 +0800

    新建 cloudalibaba-provider-payment9003/9004

---
+++ cloudalibaba-provider-payment9004/src/main/java/com/rongda/springCloud/PaymentMain9004.java
@@ -0,0 +1,17 @@
+package com.rongda.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lunrongda
+ * @create 2021-07-04 15:56
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9004.class, args);
+    }
+}
